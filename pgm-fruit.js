b4441f0548c0112638ff7efe7ff06d874afca4ac
김선경 <mimcom43@gmail.com>
2025-08-08 17:24:01 +0900

    Refactor : 수정 전 소스 (주석) 삭제

--- pgm-fruit.js
+++ pgm-fruit.js
@@ -1,15 +1,8 @@
+//school.programmers.co.kr/learn/courses/30/lessons/135808?language=javascript
+// 프로그래머스 코딩테스트 - 과일 장수 javascript
 function solution(k, m, score) {
   const sortScore = score.sort((a, b) => b - a);
 
-  // 개수가 안맞아서 버려야 할 때
-  // if (score.length % m !== 0) {
-  //     const floorFilter = sortScore.filter(item => item !== sortScore[0]);
-  //     const min = floorFilter.filter(item => item === floorFilter[0]);
-  //     console.log(min);
-  //     return floorFilter[0] * m * 1;
-  // }
-
-  // 개수가 딱 맞을 때
   const temp = [];
   const newArr = [];
   for (let i = 0; i < sortScore.length; i++) {
